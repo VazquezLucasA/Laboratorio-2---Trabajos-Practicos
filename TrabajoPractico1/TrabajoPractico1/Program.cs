634b8ff5627ffb29ee85c683c5c6409ac0363a25
Lucas Vazquez <vazquez.lucas.alejo@gmail.com>
2022-09-27 20:50:01 -0300

    tp1 aun sin terminar

---
+++ TrabajoPractico1/TrabajoPractico1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TrabajoPractico1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Universidad Tecnológica Nacional - Facultad Regional Tucumán. Septiembre 2022.
+            //Tecnicatura Universitaria en programación. Laboratorio de computación 2.
+            //Trabajo 
+            //Vázquez, Lucas Alejo. DNI 42467088.Legajo 55893.
+                        Vehiculo();
+        }
+    }
+}
